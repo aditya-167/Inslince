543a8bdff5b78cc0ec4ff9c81ffa8c369de28c15
aditya-167 <adityasrichandan3098@gmail.com>
2021-01-29 12:02:34 +0530

    application imporvement changes in Main

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,24 +1,36 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
+	
+	private static Stage stg;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			stg = primaryStage;
+			primaryStage.setResizable(false);
+			Parent root = FXMLLoader.load(getClass().getResource("view/fxmlfiles/Inslience.fxml"));
+			Scene scene = new Scene(root,600,600);
+			primaryStage.setTitle("Simple app");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public void changeScene(String fxml) throws IOException{
+		Parent pane = FXMLLoader.load(getClass().getResource(fxml));
+		stg.getScene().setRoot(pane);
 	}
 	
 	public static void main(String[] args) {
